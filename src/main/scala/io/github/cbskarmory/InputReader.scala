7551c19145e05b5344df10182f43c648791053d7
CBSkarmory <cbskarmory@users.noreply.github.com>
2019-01-23 22:36:45 -0500

    track seen exprs in main

--- src/main/scala/io/github/cbskarmory/InputReader.scala
+++ src/main/scala/io/github/cbskarmory/InputReader.scala
@@ -4,17 +4,20 @@ import io.github.cbskarmory.Lexer.tokenize
 import io.github.cbskarmory.Parser.parseExpr
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.StdIn.readLine
 
 // $COVERAGE-OFF$
 object InputReader {
     def main(args: Array[String]): Unit = {
+        val seen = mutable.HashSet[Expr]()
         @tailrec
         def readInput(): Unit = {
             val ln = readLine().toLowerCase()
             //handle input
             try {
-                println(parseExpr(tokenize(ln.toList)))
+                val exp = parseExpr(tokenize(ln.toList))
+                print(if (seen.contains(exp)) "already seen\n" else {seen.add(exp); exp + "\n"})
             } catch {
                 case e: InterpreterError => println(e.toString)
             }
